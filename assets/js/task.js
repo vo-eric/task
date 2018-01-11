715d57d47e700f7fd6a897c4621365d9b5ab0e23
Eric Vo <vo.eric@gmail.com>
2018-01-11 10:10:58 -0500

    add appending and completion/deletion of new tasks

--- assets/js/task.js
+++ assets/js/task.js
@@ -1,10 +1,20 @@
 //Deals with completion of task
-$("li").click(function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 
 //Deletes the task
-$("span").click(function(event) {
-  $(this).parent().remove();
+$("ul").on("click", "span", function(event) {
+  $(this).parent().fadeOut(function(){
+    $(this).remove();
+  });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    let newTask = $(this).val();
+    $(this).val('');
+    $("ul").append(`<li><span>X</span> ${newTodo}</li>`);
+  }
 });
